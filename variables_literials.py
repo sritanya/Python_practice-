41b9069ede275e644c3977f52ee86166473accd1
sritanya <surya@SURYASANA.local>
2024-10-03 17:30:23 +0530

    This File is for variables and literials

---
+++ variables_literials.py
@@ -0,0 +1,65 @@
+# variable def :-
+        In programming, a variable is a container (storage area) to hold data
+        we use the assignment operator = to assign a value to a variable.
+
+# Assigining values,Mutiple values ,Changing values, multiple variable same value
+
+site_name ='programmiz.com'
+print(site_name)
+  # output programmiz.com
+
+  a,b,c = 5,8.0,'chaitanya'
+  print(5)
+  print(b)
+  print(c)
+   # output 5
+   # 8.0 
+   # chaitanya"""
+
+ site_name='chaitanya'
+ print(site_name)
+   # output  chaitanya
+
+   x=y=z="cahaitanya"
+   print(x)
+   print(y)
+   print(z)
+   # output chaitanya
+   # chaitanya
+   # chaitanya
+     
+# literials def:-
+Literals are representations of fixed values in a program. They can be numbers, characters, or strings, etc. For exa, 'Hello, World!', 12, 23.0, 'C'. Literals are often used to assign values to variables or constants
+  
+  # Numeric literials
+# integer :- 
+ (2,,-9,0) ,float  (6.0,-8.9,0.0), complex (3+8j,8-5j)
+# String  :-
+ "This is a string."
+# bollean:-   
+        is_pass = True
+        print(is_pass)  
+   # Output: True   
+
+# character:-
+   some_character = 'S'
+   print (some_character)
+   # output s
+# Special :- 
+    some = None
+   print (some)
+   # Output: None
+# Collection :-
+
+ # list 
+fruits = ["apple", "mango", "orange"] 
+print(fruits)
+# tuple
+numbers = (1, 2, 3) 
+print(numbers)
+# dictionary 
+alphabets = {'a':'apple', 'b':'ball', 'c':'cat'} 
+print(alphabets)
+# set 
+vowels = {'a', 'e', 'i' , 'o', 'u'} 
+print(vowels)
