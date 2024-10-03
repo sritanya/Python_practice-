96a795bff70bd6e27b2c135c7bec262de781253a
sritanya <surya@SURYASANA.local>
2024-10-03 18:41:13 +0530

    This File is have Basic_input_output syntax in python

---
+++ Basic_input_output.py
@@ -0,0 +1,27 @@
+print('Python is powerful')
+x=10
+y=20
+print('The value of x is {} and y is {}'.format(x,y))
+
+# Output: Python is powerful
+
+# Syntax of print()
+
+# print(object= separator= end= file= flush=)
+""" Here,
+
+object - value(s) to be printed
+sep (optional) - allows us to separate multiple objects inside print().
+end (optional) - allows us to add add specific values like new line "\n", tab "\t"
+file (optional) - where the values are printed. It's default value is sys.stdout (screen)
+flush (optional) - boolean specifying if the output is flushed or buffered. Default: False """
+
+# Python Input
+
+# using input we can give the data
+num=int(input("Enter a number"))
+num=(input("enter a number"))
+print("you entered",num)
+print("datatype of the num",type(num))
+
+
